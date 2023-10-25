803477492d7d096d7e5cf271e015f3b804626359
Akshat Mittal <akshatmittal2506@gmail.com>
2023-10-25 16:10:48 +0530

    remove duplicates from dataset

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ data = pd.read_csv('dataset.csv')
 
 # print(data)
 clean_data = data.dropna()
-clean_data = pd.to_datetime(clean_data['Date'])
+clean_data = clean_data.drop_duplicates()
+# clean_data = pd.to_datetime(clean_data['Date'])
 
 print(clean_data)
